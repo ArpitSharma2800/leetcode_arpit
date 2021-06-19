0b90291ac71ede6297269d28db948035a23a02b1
ArpitSharma2800 <51937952+ArpitSharma2800@users.noreply.github.com>
2021-06-19 09:51:26 +0530

    generate paranthese

---
+++ generatePrantheses.py
@@ -0,0 +1,30 @@
+# Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+# Input: n = 3
+# Output: ["((()))","(()())","(())()","()(())","()()()"]
+# Back Tracking
+
+from typing import List
+
+
+def generateParenthesis(n: int) -> List[str]:
+    stack = []
+    res = []
+
+    def backtraking(open, closed):
+        if open == closed == n:
+            res.append("".join(stack))
+            return
+        if open < n:
+            stack.append("(")
+            backtraking(open + 1, closed)
+            stack.pop()
+        if closed < open:
+            stack.append(")")
+            backtraking(open, closed + 1)
+            stack.pop()  # after backtracking function return
+
+    backtraking(0, 0)
+    return res
+
+
+generateParenthesis(2)
