2c86babe24f51abaa04b8dcccb84802ea146de8f
ArpitSharma2800 <51937952+ArpitSharma2800@users.noreply.github.com>
2021-06-23 23:11:37 +0530

    equalsumpartition

--- equalSumPartiton.py
+++ equalSumPartiton.py
@@ -1,4 +1,22 @@
-def subSum(arr, n, sum):
+from typing import List
+
+
+def canPartition(nums: List[int]) -> bool:
+    n = len(nums)
+    sum = 0
+    for i in nums:
+        sum = sum + i
+    print(sum % 2)
+    if (sum % 2) == 0:
+        if(partition(nums, n, int(sum/2)) == True):
+            return(True)
+        else:
+            return(False)
+    else:
+        return(False)
+
+
+def partition(arr, n, sum):
     sub = ([[False for i in range(sum + 1)]
             for i in range(n + 1)])
     for i in range(n + 1):
@@ -7,20 +25,17 @@ def subSum(arr, n, sum):
         sub[0][i] = False
 
     for i in range(1, n+1):
-        for j in range(1, sum + 1):
+        for j in range(sum+1):
             if arr[i-1] <= j:
                 sub[i][j] = sub[i-1][j-arr[i-1]] or sub[i-1][j]
             else:
                 sub[i][j] = sub[i-1][j]
-
     return sub[n][sum]
 
 
 if __name__ == '__main__':
-    arr = [1, 2, 5]
-    sum = 4
-    n = len(arr)
-    if (subSum(arr, n, sum) == True):
+    arr = [1, 5, 11, 5]
+    if (canPartition(arr) == True):
         print("true")
     else:
         print("false")
